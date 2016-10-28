97fae589ef24c8be6b8da85444c91fc8ac4f07fb
Hung Truong <hung@hung-truong.com>
2016-10-27 18:32:19 -0700

    Initial Commit

---
+++ TouchFart/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TouchFart
+//
+//  Created by Hung Truong on 10/27/16.
+//  Copyright © 2016 Hung Truong. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TouchFart/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchFart
+//
+//  Created by Hung Truong on 10/27/16.
+//  Copyright © 2016 Hung Truong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
